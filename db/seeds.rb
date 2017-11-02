762c0b0d6910fa90e3306bb990dbd2e0f4d00a11
sakurashiki <qq.kwkw@gmail.com>
2017-11-02 17:33:22 +0900

    Add seed script to store default user & app keys.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+screen_names  = Rails.application.secrets.first_twitter_users.split(',')
+access_tokens = Rails.application.secrets.default_acccess_tokens
+
+p 'Start: AccessToken'
+access_tokens.split(',').each do |line|
+  access_token        = line.split(':')[0]
+  access_token_secret = line.split(':')[1]
+  if AccessToken.find_by(access_token: access_token)
+    p "Skip: #{access_token} - #{access_token_secret}"
+    next
+  end
+  p "Create: #{access_token} - #{access_token_secret}"
+  AccessToken.create(
+    access_token:        access_token,
+    access_token_secret: access_token_secret
+  )
+end
+
+p 'Start: User'
+client = User.new.connected_by(AccessToken.first.id).client
+screen_names.each do |screen_name|
+  twitter = client.user(screen_name)
+  p "Store: #{twitter.screen_name} - #{twitter.name}"
+  User.store_from_twitter(twitter, data_status: User::GOOD_STATE)
+end
